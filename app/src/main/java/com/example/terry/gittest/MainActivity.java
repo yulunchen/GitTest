73d8bffb9d65cc9b3a040bc9bf8bf06815963cbb
Yulun Chen <kitakawa@seed.net.tw>
2017-08-19 00:37:46 +0800

    加入按鈕

--- app/src/main/java/com/example/terry/gittest/MainActivity.java
+++ app/src/main/java/com/example/terry/gittest/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.terry.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button=(Button)findViewById(R.id.button);
+        textView=(TextView)findViewById(R.id.textView);
+    }
+
+    public void put(View v){
+        textView.setText("世界您好");
     }
 }
